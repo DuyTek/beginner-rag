b91e958e6150da170cce9ce19aadf5349e537acd
duynguyen-kun <duy.dnguyen@katalon.com>
2025-03-28 14:14:43 +0700

    Remove unused folders

--- django-tutorial/hello/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class React(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
-    
-    def __str__(self):
-        return self.name
