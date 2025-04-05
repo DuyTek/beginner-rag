eff277128004f06ef9639281183c2b6d8512c85f
duynguyen-kun <duy.dnguyen@katalon.com>
2025-04-05 16:12:14 +0700

    Remove constants file

--- xpathr/service/constants.py
+++
@@ -1,2 +0,0 @@
-# First endpoint
-hello = '/hello'
--- xpathr/service/routes/__init__.py
+++ xpathr/service/routes/__init__.py
@@ -1,7 +1,7 @@
-# routes/__init__.py
 from .test_scenario import test_scenario_bp
+
+SCENARIO = '/api/scenario'
 
 
 def register_routes(app):
-    """Register all blueprint routes with the app"""
-    app.register_blueprint(test_scenario_bp, url_prefix='/api/scenario')
+    app.register_blueprint(test_scenario_bp, url_prefix=SCENARIO)
